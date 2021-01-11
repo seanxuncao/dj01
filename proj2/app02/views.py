280b4b0696d2fd6c6e8a40f197d412ee5874c33a
Your Name <you@example.com>
2021-01-11 14:35:48 -0500

    app02

---
+++ proj2/app02/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+# Create your views here.
+
+
+def home(request):
+    return HttpResponseRedirect("static/index.html")
